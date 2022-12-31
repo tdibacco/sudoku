7007158fc3b0d4b01209847707a5ad0b8272559c
Trent DiBacco <trent@Trents-MacBook-Pro.local>
2022-12-31 13:35:16 -0800

    init sudoku.

---
+++ cmd/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	sudoku "sudoku/internal"
+)
+
+func main() {
+	input := sudoku.Grid{
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+	}
+
+	if sudoku.Solve(&input) {
+		sudoku.Print(&input)
+	} else {
+		println("Not Valid")
+	}
+}
